0587b2eaddf543be7390cb113bca2390ddf3674f
Pei-Hsuan Lin <alice70106@gmail.com>
2023-11-26 16:00:00 -0800

    add env file and CLIP transformer

--- CLIP.py
+++ CLIP.py
@@ -1,18 +1,19 @@
-import dataloader
-import random
 from torchvision import transforms
-from PIL import Image
 from transformers import CLIPProcessor, CLIPModel
+from PIL import Image
+import requests
 food_train_transforms = transforms.Compose(
     [
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
     ]
 )
-train_set, val_set = dataloader.get_dataset(food_train_transforms)
+# train_set, val_set = dataloader.get_dataset(food_train_transforms)
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-num_images = len(val_set)
-random_index = random.randint(0, num_images - 1)
-random_image, label = val_set[random_index] 
-image = Image.open(random_image)
+url = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Xiaolongbao-breakfast.jpg/460px-Xiaolongbao-breakfast.jpg"
+image = Image.open(requests.get(url, stream=True).raw)
+input = processor(text=["a image of an apple pie", "a image of dumplings"], images=image, return_tensors="pt", padding=True)
+output = model(**input)
+probs = output.logits_per_image.softmax(dim=1)
+print(probs)
--- dataloader.py
+++ dataloader.py
@@ -1,14 +1,20 @@
-from torchvision import datasets
-from torchvision import transforms
-# print(dir(datasets))
+from torchvision.datasets import ImageFolder
+from torch.utils.data import random_split
+from math import floor
+
 def get_dataset(transform):
-    train_set = datasets.Food101(
-        root="./Data", download=True, transform=transform
-        , split="train"
-    )
-    val_set = datasets.Food101(
-        root="./Data", download=True, transform=transform
-        , split="test"
-    )
-    return train_set, val_set
+    # train_set = datasets.Food101(
+    #     root="./Data", download=True, transform=transform
+    #     , split="train"
+    # )
+    # val_set = datasets.Food101(
+    #     root="./Data", download=True, transform=transform
+    #     , split="test"
+    # )
+    # return train_set, val_set
+    dataset = ImageFolder("./Data/food-101/images", transform)
+    val_size = floor((len(dataset)) * 0.10)
+    train_size = len(dataset) - val_size
+    train_ds, val_ds = random_split(dataset, [train_size, val_size])
+    return train_ds, val_ds
 

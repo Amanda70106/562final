a169c61a3f2dd88889e4a2ebb5ccf0835314f20a
Pei-Hsuan Lin <alice70106@gmail.com>
2023-12-12 23:13:53 -0800

    modify attention map

--- attentionMap.py
+++ attentionMap.py
@@ -6,9 +6,10 @@ import numpy as np
 import cv2
 import os
 from transformers import CLIPProcessor, CLIPModel
+from load_model import load_deit_model
 
 
-def rollout(attentions, discard_ratio, head_fusion):
+def rollout(attentions, discard_ratio):
     result = torch.eye(attentions[0].size(-1))
     with torch.no_grad():
         for attention in attentions:
@@ -17,11 +18,9 @@ def rollout(attentions, discard_ratio, head_fusion):
             _, indices = flat.topk(int(flat.size(-1)*discard_ratio), -1, False)
             indices = indices[indices != 0]
             flat[0, indices] = 0
-
             I = torch.eye(attention_heads_fused.size(-1))
             a = (attention_heads_fused + 1.0*I)/2
             a = a / a.sum(dim=-1)
-
             result = torch.matmul(a, result)
     
 
@@ -31,11 +30,9 @@ def rollout(attentions, discard_ratio, head_fusion):
     mask = mask / np.max(mask)
     return mask    
 
-class VITAttentionRollout:
-    def __init__(self, model, attention_layer_name='attn_drop', head_fusion="mean",
-        discard_ratio=0.9):
+class Rollout:
+    def __init__(self, model, attention_layer_name='attn_drop', discard_ratio=0.8):
         self.model = model
-        self.head_fusion = head_fusion
         self.discard_ratio = discard_ratio
         for name, module in self.model.named_modules():
             if attention_layer_name in name:
@@ -55,7 +52,7 @@ class VITAttentionRollout:
         ])
         input_tensor = transform(image).unsqueeze(0)
         if model_name == "CLIP":
-            processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
+            processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32", output_attentions=True)
             input = processor(text=['Apple pie', 'Baby back ribs', 'Baklava', 'Beef carpaccio', 'Beef tartare', 
                             'Beet salad', 'Beignets', 'Bibimbap', 'Bread pudding', 'Breakfast burrito', 
                             'Bruschetta', 'Caesar salad', 'Cannoli', 'Caprese salad', 'Carrot cake', 
@@ -77,15 +74,13 @@ class VITAttentionRollout:
                             'Spaghetti carbonara', 'Spring rolls', 'Steak', 'Strawberry shortcake', 
                             'Sushi', 'Tacos', 'Takoyaki', 'Tiramisu', 'Tuna tartare', 'Waffles'], 
                             images=image, return_tensors="pt", padding=True)
-            with torch.no_grad():
-                output = self.model(**input)
-                self.attentions = output.vision_model_output.attentions
-            print(type(self.attentions))
+            output = self.model(**input)
+            self.attentions = output.vision_model_output.attentions
         elif model_name == "deit":
             with torch.no_grad():
                 output = self.model(input_tensor)
 
-        return rollout(self.attentions, self.discard_ratio, self.head_fusion)
+        return rollout(self.attentions, self.discard_ratio)
     
 
 
@@ -99,29 +94,19 @@ def show_mask_on_image(img, mask):
     return np.uint8(255 * cam)
 
 if __name__ == '__main__':
-    # model = torch.hub.load('facebookresearch/deit:main', 
-    #     'deit_tiny_patch16_224', pretrained=True)
-    # model.eval()
-    model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32", output_attentions=True)
-
-    img_path = os.path.join(os.path.dirname(__file__), "Data", "food-101", "images", "apple_pie", "280007.jpg")
-    # img_path = os.path.join(os.path.dirname(__file__), "plane.png")
-    print(img_path)
+    model = load_deit_model()
+    model.eval()
+    # model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32", output_attentions=True)
+    img_path = os.path.join(os.path.dirname(__file__), "Data", "food-101", "images", "pulled_pork_sandwich", "228988.jpg")
     img = Image.open(img_path)
     img = img.resize((224, 224))
-
-    print("Doing Attention Rollout")
-    attention_rollout = VITAttentionRollout(model)
-    mask = attention_rollout(img, "CLIP")
-    name = "attention_rollout_{:.3f}_{}.png".format(0.9, "mean")
+    attention_rollout = Rollout(model)
+    mask = attention_rollout(img, "deit")
+    name = "attention_rollout_{:.3f}_{}.png".format(0.8, "mean")
 
 
 
     np_img = np.array(img)[:, :, ::-1]
     mask = cv2.resize(mask, (np_img.shape[1], np_img.shape[0]))
     mask = show_mask_on_image(np_img, mask)
-    cv2.imshow("Input Image", np_img)
-    cv2.imshow(name, mask)
-    cv2.imwrite("input.png", np_img)
     cv2.imwrite(name, mask)
-    # cv2.waitKey(-1)
